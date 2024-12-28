268af4fb8bd3a15ea371fb5dc3eb7a7786dba4b2
arti kaintura <artikaintura274@gmail.com>
2024-12-28 18:10:30 +0530

    added

--- Backend/server.js
+++ Backend/server.js
@@ -52,7 +52,7 @@ app.post("/contact", async (req, res) => {
 });
 
 // Start server on port 5000
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () =>{
   console.log(`Server running on port ${PORT}`);
 });
